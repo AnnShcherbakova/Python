331850bbf34167d6de017e843e8710a01363e375
Hanna <aneckaanuyta08031993@gmail.com>
2023-07-24 08:36:54 +0300

    1

---
+++ TestSelenium.py
@@ -0,0 +1,32 @@
+import requests as requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+link = "http://34.141.58.52:8080/#/login"
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element(By.ID, "login")
+    input1.send_keys("www.anuyta@mail.ru")
+
+    time.sleep(5)
+    input2 = browser.find_element(By.CSS_SELECTOR, '#password > input')
+    input2.send_keys("987654321qaqa")
+    time.sleep(5)
+
+    button = browser.find_element(By.CLASS_NAME, 'p-button-label')
+    button.submit()
+    time.sleep(10)
+
+    response = requests.get('http://34.141.58.52:8080/#/profile')
+
+    r = response.status_code
+
+    assert response.status_code == 200
+    browser.save_screenshot('result3.png')
+
+finally:
+    browser.quit()
